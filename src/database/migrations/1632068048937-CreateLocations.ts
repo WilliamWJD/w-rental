cd3b25b2c97bac1a924ec0dd400ae6312e32f894
williamwjd <william.jose.wjd@gmail.com>
2021-09-19 13:26:19 -0300

    criando migration locations

---
+++ src/database/migrations/1632068048937-CreateLocations.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateLocations1632068048937 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"locations",
+                columns: [
+                    {
+                        name:"id",
+                        type:"uuid",
+                        isPrimary:true
+                    },
+                    {
+                        name:"data_start",
+                        type:"date",
+                        isNullable:false
+                    },
+                    {
+                        name:"data_end",
+                        type:"date",
+                        isNullable:false
+                    },
+                    {
+                        name:"house_id",
+                        type:"uuid"
+                    },
+                    {
+                        name:"tenant_id",
+                        type:"uuid"
+                    }
+                ],
+                foreignKeys:[
+                    {
+                        name:"FKHouseLocation",
+                        referencedTableName:"houses",
+                        referencedColumnNames:["id"],
+                        columnNames:["house_id"],
+                        onDelete:"SET NULL",
+                        onUpdate:"SET NULL"
+                    },
+                    {
+                        name:"FKTenantLocation",
+                        referencedTableName:"tenants",
+                        referencedColumnNames:["id"],
+                        columnNames:["tenant_id"],
+                        onDelete:"SET NULL",
+                        onUpdate:"SET NULL"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("locations")
+    }
+
+}
