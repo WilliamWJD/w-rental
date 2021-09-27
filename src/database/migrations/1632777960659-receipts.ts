1c1e71ee9f539eae9afac2e1f05ec15af218a972
williamwjd <william.jose.wjd@gmail.com>
2021-09-27 19:15:07 -0300

    migration receipts

---
+++ src/database/migrations/1632777960659-receipts.ts
@@ -0,0 +1,64 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class receipts1632777960659 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:'receipts',
+                columns:[
+                    {
+                        name:"id",
+                        type:"uuid",
+                        isPrimary:true
+                    },
+                    {
+                        name:"location_id",
+                        type:"uuid"
+                    },
+                    {
+                        name:"date_start",
+                        type:"date"
+                    },
+                    {
+                        name:"date_end",
+                        type:"date"
+                    },
+                    {
+                        name:"energy_value",
+                        type:"numeric"
+                    },
+                    {
+                        name:"water_value",
+                        type:"numeric"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+                foreignKeys:[
+                    {
+                        name:"FKLocationReceipt",
+                        referencedTableName:"locations",
+                        referencedColumnNames:["id"],
+                        columnNames:["location_id"],
+                        onDelete:"SET NULL",
+                        onUpdate:"SET NULL"
+                    },
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('receipts')
+    }
+
+}
