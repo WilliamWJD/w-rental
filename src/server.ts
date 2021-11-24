bf5327e2cf9c7afd15e1d6e84dc77a9ef096a04a
williamwjd <william.jose.wjd@gmail.com>
2021-11-23 21:33:00 -0300

    configurando cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import "reflect-metadata";
 import './database';
 import 'dotenv/config';
@@ -6,6 +7,7 @@ import express from 'express';
 import { routes } from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
